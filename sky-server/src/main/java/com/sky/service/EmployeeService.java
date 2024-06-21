22ee3ae310241b51977decfdada20130c7705f4c
appltin <redloveandten@gmail.com>
2024-06-21 16:26:44 +0800

    增加分頁功能

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -12,6 +14,8 @@ public interface EmployeeService {
      * @return
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
-
+    //新增員工
     void save(EmployeeDTO employeeDTO);
+    //分頁查詢
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
